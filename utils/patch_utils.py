5c136b4c86208c434d1d27fb8e77ab1fc1ddb888
Ubuntu <ubuntu@ip-172-31-26-1.eu-central-1.compute.internal>
2024-06-29 08:17:03 +0000

    ENH: add argument to put patch into rgb before classif

--- utils/patch_utils.py
+++ utils/patch_utils.py
@@ -45,7 +45,7 @@ def is_annot_center_in_patch(x, y, size):
     return ((0 < x) and (0 < y) and (x < size) and (y < size))
 
 
-def create_img_to_classif_from_box(pred_box, cut, box_size):
+def create_img_to_classif_from_box(pred_box, cut, box_size, to_rgb=True):
     x0, y0, x1, y1 = np.array(pred_box).astype(int)
     center = (x0 + x1) // 2, (y0 + y1) // 2
     x0, y0 = center[0] - box_size // 2, center[1] - box_size // 2
@@ -60,6 +60,7 @@ def create_img_to_classif_from_box(pred_box, cut, box_size):
     if img_to_classif.shape != (box_size, box_size, 3):
         print(img_to_classif.shape)
         img_to_classif = cv2.resize(img_to_classif, (box_size, box_size))
-    img_to_classif = cv2.cvtColor(img_to_classif, cv2.COLOR_BGR2RGB)
+    if to_rgb:
+        img_to_classif = cv2.cvtColor(img_to_classif, cv2.COLOR_BGR2RGB)
     
     return img_to_classif
